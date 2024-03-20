596fefe09dd5ec8bafa6699279efee0fe1b51c33
syio27 <baglansagidulladev@gmail.com>
2024-03-20 21:33:43 +0100

    project init

---
+++ src/main/java/com/bsn/westwood/WestwoodApplication.java
@@ -0,0 +1,13 @@
+package com.bsn.westwood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestwoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestwoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsn/westwood/WestwoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsn.westwood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestwoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
